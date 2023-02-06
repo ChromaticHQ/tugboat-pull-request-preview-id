3244e67d2e4db21b8810d777c2db5469b664f7ba
Mark Dorison <mark@chromatichq.com>
2023-02-06 15:02:40 -0500

    Added exit code if preview id is not retrieved.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,12 @@
 
 export TUGBOAT_API_TOKEN=$1
 
-tugboat ls previews repo=$2 -j | jq -r "first(.[] | select(.ref == \"pr$3\") | .id)" | sed 's/^/tugboat-preview-id=/' | cat >> $GITHUB_OUTPUT
+export GITHUB_OUTPUT=$GITHUB_OUTPUT$(tugboat ls previews repo=$2 -j | jq -r "first(.[] | select(.ref == \"pr$3\") | .id)" | sed 's/^/tugboat-preview-id=/')
+
+if [ -z "$GITHUB_OUTPUT" ]
+then
+  echo "\$GITHUB_OUTPUT is empty"
+  exit 1
+else
+  echo "\$GITHUB_OUTPUT is NOT empty"
+fi
