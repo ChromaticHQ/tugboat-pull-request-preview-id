9b1ba9017bdb3cba7b9afe502850008c51b2dd1a
Mark Dorison <mark@chromatichq.com>
2023-02-07 17:14:53 -0500

    Remove debug echo.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,6 @@ export TUGBOAT_API_TOKEN=$1
 
 tugboat ls previews repo=$2 -j | jq -r "first(.[] | select(.ref == \"pr$3\") | .id)" | sed 's/^/tugboat-preview-id=/' | cat >> $GITHUB_OUTPUT
 
-echo $GITHUB_OUTPUT
-
 if [ -z "$GITHUB_OUTPUT" ]
 then
   echo "\$GITHUB_OUTPUT is empty"
